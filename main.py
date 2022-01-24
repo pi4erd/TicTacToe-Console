ce7b60f401101d7638370463f3e70224d998f9b4
DeviDesktop <piuuttri@gmail.com>
2022-01-24 22:07:53 +0200

    wow, so many commits

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def player_input(difficulty):
             print("Это был неправильный ход. Попробуйте еще раз!")
             input()
 
-# TODO: #1 Make graphics or html or something, idk
+# TODO: Make graphics or html or something, idk
 
 while True:
     clr_scr()
